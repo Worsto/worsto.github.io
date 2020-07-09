a12a472f9cbd55dfe0851c8d05f6d584b35ec063
Worsto <worsto.inbox@gmail.com>
2020-07-09 16:57:03 +0300

    Продолжаем продолжать

--- js/script.js
+++ js/script.js
@@ -30,23 +30,45 @@ for (let i = 0; i < sliderButtons.length; i++) {
 sliderBackward.addEventListener("click", function (evt) {
     evt.preventDefault();
     for (let i = 0; i < sliderInformation.length; i++) {
+        let classCountFix = i - 1;
+        let firstSlidejumper = sliderInformation.length - 1;
         if (sliderInformation[i].classList.contains("slide-current")) {
-            if (i = 0) {
-                slideKiller(sliderButtons, sliderInformation, sliderBg);
-                let arrCounter = sliderInformation.length - 1;
-                sliderBg.classList.add("slider-" + sliderInformation.length);
-                sliderButtons[arrCounter].classList.add("current");
-                sliderInformation[arrCounter].classList.add("slide-current");
-            } else {       
-                slideKiller(sliderButtons, sliderInformation, sliderBg);
-                let classCount = i - 1;
+            slideKiller(sliderButtons, sliderInformation, sliderBg);
+            if (i !== 0) {
                 sliderBg.classList.add("slider-" + i);
-                sliderButtons[classCount].classList.add("current");
-                sliderInformation[classCount].classList.add("slide-current");
+                sliderButtons[classCountFix].classList.add("current");
+                sliderInformation[classCountFix].classList.add("slide-current");
+            } else {
+                sliderBg.classList.add("slider-" + sliderInformation.length);
+                sliderButtons[firstSlidejumper].classList.add("current");
+                sliderInformation[firstSlidejumper].classList.add("slide-current");
+                break;
             }
         }
     }
-})
+});
+
+sliderForward.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    for (let i = 0; i < sliderInformation.length; i++) {
+        let firstSlidejumper = sliderInformation.length - 1;
+        let nextSlideBg = i + 2;
+        let nextSlide = i + 1;
+        if (sliderInformation[i].classList.contains("slide-current")) {
+            slideKiller(sliderButtons, sliderInformation, sliderBg);
+            if (i !== firstSlidejumper) {
+                sliderBg.classList.add("slider-" + nextSlideBg);
+                sliderButtons[nextSlide].classList.add("current");
+                sliderInformation[nextSlide].classList.add("slide-current");
+                break;
+            } else {
+                sliderBg.classList.add("slider-" + 1);
+                sliderButtons[0].classList.add("current");
+                sliderInformation[0].classList.add("slide-current");
+            }
+        }
+    }
+});
 
 // Табы
 
